20c3298240e0cf9f75a097442feac143bcca8866
Yalcin Imeryuz <yimeryuz@gmail.com>
2021-01-16 15:02:46 +0400

    Initialize Commit

--- app.py
+++ app.py
@@ -31,9 +31,31 @@ def draw_page():
     def create_figure():
         fig = Figure()
         axis = fig.add_subplot(1, 1, 1)
-        xs = range(int(request.form['GraphXValues']))
-        ys = [np.sin(x) for x in xs]
-        axis.plot(xs, ys)
+        if ',' not in request.form['GraphXValues']:
+            xs = range(int(request.form['GraphXValues']))
+            if request.form['GraphYValues'] == 'sin(x)':
+                ys = [np.sin(x) for x in xs]
+                axis.plot(xs, ys)
+            elif request.form['GraphYValues'] == '2x':
+                ys = [x * 2 for x in xs]
+                axis.plot(xs, ys)
+            elif request.form['GraphYValues'] == 'x^2+2x+24':
+                xs = range(-int(request.form['GraphXValues']), int(request.form['GraphXValues']))
+                ys = [x**2+(2*x)+24 for x in xs]
+                print(ys)
+                axis.plot(xs, ys)
+            elif request.form['GraphYValues'] == 'x^3+2x^2+3x+25':
+                xs = range(-int(request.form['GraphXValues']), int(request.form['GraphXValues']))
+                ys = [x**3 + (2 * x**2) + (3 * x) + 25 for x in xs]
+                axis.plot(xs, ys)
+            elif request.form['GraphYValues'] == 'cos(x)':
+                xs = range(-int(request.form['GraphXValues']), int(request.form['GraphXValues']))
+                ys = [np.cos(x) for x in xs]
+                axis.plot(xs, ys)
+            elif request.form['GraphYValues'] == 'tan(x)':
+                xs = range(-int(request.form['GraphXValues']), int(request.form['GraphXValues']))
+                ys = [np.tan(x) for x in xs]
+                axis.plot(xs, ys)
         return fig
     
     if request.method == 'POST':
