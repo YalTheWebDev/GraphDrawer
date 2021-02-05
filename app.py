e9b27729f4e88c40bfe041c1f4dc3911fabc138c
Yalcin Imeryuz <yimeryuz@gmail.com>
2021-02-05 20:22:00 +0400

    Adding support for x^4 and exponential graphs.

--- app.py
+++ app.py
@@ -32,48 +32,57 @@ def draw_page():
         fig = Figure()
         axis = fig.add_subplot(1, 1, 1)
         # Handling different graph types (incomplete)
-        if ',' not in request.form['GraphXValues']:
-            xs = range(int(request.form['GraphXValues']))
-            if request.form['GraphYValues'] == 'sin(x)':
-                ys = [np.sin(x) for x in xs]
-                axis.plot(xs, ys)
-            elif 'x' in request.form['GraphYValues'] and '^2' not in request.form['GraphYValues'] and '^3' not in request.form['GraphYValues']:
-                if '+' in request.form['GraphYValues']:
-                    values = request.form['GraphYValues'].split('+')
-                    values = [value.split('x') for value in values]
-                    values = [values[0][0], values[1][0]]
-                    print(values)
-                    ys = [x * values[0] + values[1][0] for x in xs]
-                    axis.plot(xs, ys)
-            elif request.form['GraphYValues'] == 'x^2+2x+24':
-                xs = range(-int(request.form['GraphXValues']), int(request.form['GraphXValues']))
-                ys = [x**2+(2*x)+24 for x in xs]
-                print(ys)
-                axis.plot(xs, ys)
-            elif 'x^3' in request.form['GraphYValues']:
+        xs = range(int(request.form['GraphXValues']))
+        if request.form['GraphYValues'] == 'sin(x)':
+            ys = [np.sin(x) for x in xs]
+            axis.plot(xs, ys)
+        elif 'x' in request.form['GraphYValues'] and '^2' not in request.form['GraphYValues'] and '^3' not in request.form['GraphYValues'] and 'sin' not in request.form['GraphYValues'] and 'cos' not in request.form['GraphYValues'] and 'tan' not in request.form['GraphYValues'] and request.form['GraphYValues'] != '1/x' and request.form['GraphYValues'] != '2^x' and 'x^4' not in request.form['GraphYValues']:
+            if '+' in request.form['GraphYValues']:
                 values = request.form['GraphYValues'].split('+')
                 values = [value.split('x') for value in values]
+                values = [values[0][0], values[1][0]]
                 print(values)
-                values = [values[0][0], int(values[1][0]), int(values[2][0]), int(values[3][0])]
-                print(values)
-                xs = range(-int(request.form['GraphXValues']), int(request.form['GraphXValues']))
-                ys = [x**3 + (values[1] * x**2) + (values[2] * x) + values[3] for x in xs]
+                ys = [x * values[0] + values[1][0] for x in xs]
                 axis.plot(xs, ys)
-            elif request.form['GraphYValues'] == 'cos(x)':
-                xs = range(-int(request.form['GraphXValues']), int(request.form['GraphXValues']))
-                ys = [np.cos(x) for x in xs]
-                axis.plot(xs, ys)
-            elif request.form['GraphYValues'] == 'tan(x)':
-                xs = range(-int(request.form['GraphXValues']), int(request.form['GraphXValues']))
-                ys = [np.tan(x) for x in xs]
-                axis.plot(xs, ys)
-            elif request.form['GraphYValues'] == '1/x':
-                xs = range(-int(request.form['GraphXValues']), int(request.form['GraphXValues']))
-                xs = list(xs)
-                xs.remove(0)
-                ys = [1/x for x in xs]
-                axis.plot(xs, ys)
-                print(xs, ys)
+        elif request.form['GraphYValues'] == 'x^2+2x+24':
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+            ys = [x**2+(2*x)+24 for x in xs]
+            print(ys)
+            axis.plot(xs, ys)
+        elif 'x^3' in request.form['GraphYValues'] and 'x^4' not in request.form['GraphYValues']:
+            values = request.form['GraphYValues'].split('+')
+            values = [value.split('x') for value in values]
+            print(values)
+            values = [values[0][0], int(values[1][0]), int(values[2][0]), int(values[3][0])]
+            print(values)
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+            ys = [x**3 + (values[1] * x**2) + (values[2] * x) + values[3] for x in xs]
+            axis.plot(xs, ys)
+        elif request.form['GraphYValues'] == 'cos(x)':
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+            ys = [np.cos(x) for x in xs]
+            axis.plot(xs, ys)
+        elif request.form['GraphYValues'] == 'tan(x)':
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+            ys = [np.tan(x) for x in xs]
+            axis.plot(xs, ys)
+        elif request.form['GraphYValues'] == '1/x':
+            xs = np.linspace(0, int(request.form['GraphXValues']), 1000)
+            xs = list(xs)
+            xs.remove(0)
+            ys = [1/x for x in xs]
+            axis.plot(xs, ys)
+            print(xs, ys)
+        elif request.form['GraphYValues'] == '2^x':
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+            ys = [2 ** x for x in xs]
+            axis.plot(xs, ys)
+        elif 'x^4' in request.form['GraphYValues']:
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+            values = request.form['GraphYValues'].split('+')
+            print(values)
+            ys = [x**4 for x in xs]
+            axis.plot(xs, ys)
         return fig
     
     if request.method == 'POST':
