121cc305d1c84239bd6d5af2febfc3b24e42d635
Yalcin Imeryuz <yimeryuz@gmail.com>
2021-07-29 10:10:55 +0300

    More Parabola Support
    Includes support for up to x^7. Still issues with x^2.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import io
 from flask import Flask, request, Response
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+from sqlite3 import Error
 from datetime import datetime
 from flask.templating import render_template
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -8,19 +9,6 @@ from matplotlib.figure import Figure
 import numpy as np
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///graphs.db'
-db = SQLAlchemy(app)
-
-
-class Grapher(db.Model):
-    GraphID =  db.Column(db.Integer, primary_key=True)
-    GraphXValues = db.Column(db.String(200), nullable=False)
-    GraphYValues = db.Column(db.String(200), nullable=False)
-    GraphURI = db.Column(db.String(20), nullable=False)
-    GraphDate = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return "<Task %r>" % self.id
 
 @app.route('/')
 def home_page():
@@ -33,6 +21,8 @@ def draw_page():
         axis = fig.add_subplot(1, 1, 1)
         # Handling different graph types (incomplete)
         xs = range(int(request.form['GraphXValues']))
+        compression = int(request.form['DataCompression'])
+        compressionAmount = [1000 if compression == 0 else 1000 // (compression * 10)]
         if request.form['GraphYValues'] == 'sin(x)':
             ys = [np.sin(x) for x in xs]
             axis.plot(xs, ys)
@@ -45,43 +35,65 @@ def draw_page():
                 ys = [x * values[0] + values[1][0] for x in xs]
                 axis.plot(xs, ys)
         elif request.form['GraphYValues'] == 'x^2+2x+24':
-            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), compressionAmount)
             ys = [x**2+(2*x)+24 for x in xs]
             print(ys)
             axis.plot(xs, ys)
-        elif 'x^3' in request.form['GraphYValues'] and 'x^4' not in request.form['GraphYValues']:
+        elif 'x^3' in request.form['GraphYValues'] and 'x^4' not in request.form['GraphYValues'] and 'x^5' not in request.form['GraphYValues'] and 'x^6' not in request.form['GraphYValues'] and 'x^7' not in request.form['GraphYValues']:
             values = request.form['GraphYValues'].split('+')
             values = [value.split('x') for value in values]
             print(values)
             values = [values[0][0], int(values[1][0]), int(values[2][0]), int(values[3][0])]
             print(values)
-            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), compressionAmount[0])
             ys = [x**3 + (values[1] * x**2) + (values[2] * x) + values[3] for x in xs]
             axis.plot(xs, ys)
         elif request.form['GraphYValues'] == 'cos(x)':
-            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), compressionAmount[0])
             ys = [np.cos(x) for x in xs]
             axis.plot(xs, ys)
         elif request.form['GraphYValues'] == 'tan(x)':
-            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), compressionAmount[0])
             ys = [np.tan(x) for x in xs]
             axis.plot(xs, ys)
         elif request.form['GraphYValues'] == '1/x':
-            xs = np.linspace(0, int(request.form['GraphXValues']), 1000)
+            xs = np.linspace(0, int(request.form['GraphXValues']), compressionAmount[0])
             xs = list(xs)
             xs.remove(0)
             ys = [1/x for x in xs]
             axis.plot(xs, ys)
             print(xs, ys)
         elif request.form['GraphYValues'] == '2^x':
-            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), compressionAmount[0])
             ys = [2 ** x for x in xs]
             axis.plot(xs, ys)
-        elif 'x^4' in request.form['GraphYValues']:
-            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), 1000)
+        elif 'x^4' in request.form['GraphYValues'] and 'x^5' not in request.form['GraphYValues'] and 'x^6' not in request.form['GraphYValues'] and 'x^7' not in request.form['GraphYValues']:
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), compressionAmount[0])
             values = request.form['GraphYValues'].split('+')
+            values = [value.split('x') for value in values]
             print(values)
-            ys = [x**4 for x in xs]
+            ys = [int(values[0][0]) * x**4 + int(values[1][0]) * x**3 + int(values[2][0]) * x**2 + int(values[3][0]) * x + int(values[4][0]) for x in xs]
+            axis.plot(xs, ys)
+        elif 'x^5' in request.form['GraphYValues'] and 'x^6' not in request.form['GraphYValues'] and 'x^7' not in request.form['GraphYValues']:
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), compressionAmount[0])
+            values = request.form['GraphYValues'].split('+')
+            values = [value.split('x') for value in values]
+            print(values)
+            ys = [int(values[0][0]) * x**5 for x in xs]
+            axis.plot(xs, ys)
+        elif 'x^6' in request.form['GraphYValues'] and 'x^7' not in request.form['GraphYValues']:
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), compressionAmount[0])
+            values = request.form['GraphYValues'].split('+')
+            values = [value.split('x') for value in values]
+            print(values)
+            ys = [int(values[0][0]) * x**6 + int(values[1][0]) * x**5 + int(values[2][0]) * x**4 + int(values[3][0]) * x**3 + int(values[4][0]) * x**2 + int(values[5][0]) * x + int(values[6][0]) for x in xs]
+            axis.plot(xs, ys)
+        elif 'x^7' in request.form['GraphYValues']:
+            xs = np.linspace(-int(request.form['GraphXValues']), int(request.form['GraphXValues']), compressionAmount[0])
+            values = request.form['GraphYValues'].split('+')
+            values = [value.split('x') for value in values]
+            print(values)
+            ys = [int(values[0][0]), int(values[1][0]), int(values[2][0]), int(values[3][0]), int(values[4][0]), int(values[5][0]), int(values[6][0]), int(values[7][0])]
             axis.plot(xs, ys)
         return fig
     
@@ -90,7 +102,14 @@ def draw_page():
             fig = create_figure()
             output = io.BytesIO()
             FigureCanvas(fig).print_png(output)
-            return Response(output.getvalue(), mimetype='image/png')
+            image = Response(output.getvalue(), mimetype='image/png')
+            try:
+                connection = sqlite3.connect('graph.db')
+                cursor = connection.cursor()
+                connection.commit()
+            except Error:
+                print('Error Connecting To/Modyfing Database.')
+            return image
         except:
             return render_template('graph_error.html')
     else:
